23c8f8517d3adb6396cb2030dfcbf9b1a1211f5f
Gullyyboy <asadullahalgalib2004@gmail.com>
2026-06-27 17:26:43 +0600

    Initial commit for CSE391 Assignment 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// Display the current page URL
+document.getElementById("pageLocation").innerHTML =
+    "<strong>Page Location:</strong> " + window.location.href;
+
+// Display the last modified date
+document.getElementById("lastModified").innerHTML =
+    "<strong>Last Modified:</strong> " + document.lastModified;
